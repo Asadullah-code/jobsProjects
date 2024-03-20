e8ea24c4e6ae98fc805069dadd05932c0abf408b
Asadullah-code <80662464+Asadullah-code@users.noreply.github.com>
2024-03-21 00:24:26 +0500

    new files
    new files

---
+++ allJobs.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Job Cards</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background: #fff;">
+
+
+
+	<div class="container my-5">
+		<div class="row">
+			<div class="col-lg-4 sidebar-left">
+
+				<div class="accordion accordion-flush" id="accordionFlushExample">
+				  <div class="accordion-item">
+				    <h2 class="accordion-header">
+				      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+				        Jobs by location
+				      </button>
+				    </h2>
+				    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+				      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+				    </div>
+				  </div>
+				  <div class="accordion-item">
+				    <h2 class="accordion-header">
+				      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+				        Jobs by category
+				      </button>
+				    </h2>
+				    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+				      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+				    </div>
+				  </div>
+				  <div class="accordion-item">
+				    <h2 class="accordion-header">
+				      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+				        Jobs by type
+				      </button>
+				    </h2>
+				    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+				      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+				    </div>
+				  </div>
+				</div>
+
+			</div>
+			<div class="col-lg-8 otherJobS">
+				
+			</div>
+		</div>
+	</div>
+
+
+
+
+		
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Job Cards</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+	<div class="container my-4">
+		<!-- first row start here -->
+		<div class="row">
+			<div class="col-6 col-md-6 col-12 d-flex align-items-center justify-content-start">
+				<h5 class="trHead"><i class="bi bi-graph-up"></i> Trending jobs</h5>
+			</div>
+			<div class="col-6 col-md-6 col-12 d-flex align-items-center justify-content-end">
+				<a href="allJobs.php" class="alljBtn">View all jobs <i class="bi bi-chevron-right arrIcon"></i></a>
+			</div>
+		</div>
+		<!-- first row end here -->
+		<div class="row my-5">
+			<!-- card start here -->
+			<div class="col-xxl-4 col-lg-4 col-md-6 col-12 mb-4">
+				<div class="card areCard">
+				  <div class="card-body">
+				    <h5 class="card-title areHead">Area Sales Manager</h5>
+				    <div class="d-flex align-items-center justify-content-between mb-4">
+				    	<span class="areSpan"><i class="bi bi-geo-alt" style="margin-right: 5px;"></i> Cork</span>
+				    	<span class="areSpan"><i class="bi bi-bag" style="margin-right: 5px;"></i> Permanent</span>
+				    	<span class="areSpan"><i class="bi bi-cash-stack" style="margin-right: 5px;"></i> Competitive</span>
+				    </div>
+				    <span class="areSpanN">new</span>
+				    <div class="d-flex align-items-end justify-content-between">
+					    <span class="text-body-secondary timeSpan">56 minutes ago</span>
+					    <a href="#" class="viewBtn">view <i class="bi bi-chevron-right arrIcon ms-1"></i></a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- card end here -->
+			<!-- card start here -->
+			<div class="col-xxl-4 col-lg-4 col-md-6 col-12 mb-4">
+				<div class="card areCard">
+				  <div class="card-body">
+				    <h5 class="card-title areHead">Area Sales Manager</h5>
+				    <div class="d-flex align-items-center justify-content-between mb-4">
+				    	<span class="areSpan"><i class="bi bi-geo-alt" style="margin-right: 5px;"></i> Cork</span>
+				    	<span class="areSpan"><i class="bi bi-bag" style="margin-right: 5px;"></i> Permanent</span>
+				    	<span class="areSpan"><i class="bi bi-cash-stack" style="margin-right: 5px;"></i> Competitive</span>
+				    </div>
+				    <span class="areSpanN">new</span>
+				    <div class="d-flex align-items-end justify-content-between">
+					    <span class="text-body-secondary timeSpan">56 minutes ago</span>
+					    <a href="#" class="viewBtn">view <i class="bi bi-chevron-right arrIcon ms-1"></i></a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- card end here -->
+			<!-- card start here -->
+			<div class="col-xxl-4 col-lg-4 col-md-6 col-12 mb-4">
+				<div class="card areCard">
+				  <div class="card-body">
+				    <h5 class="card-title areHead">Area Sales Manager</h5>
+				    <div class="d-flex align-items-center justify-content-between mb-4">
+				    	<span class="areSpan"><i class="bi bi-geo-alt" style="margin-right: 5px;"></i> Cork</span>
+				    	<span class="areSpan"><i class="bi bi-bag" style="margin-right: 5px;"></i> Permanent</span>
+				    	<span class="areSpan"><i class="bi bi-cash-stack" style="margin-right: 5px;"></i> Competitive</span>
+				    </div>
+				    <span class="areSpanN">new</span>
+				    <div class="d-flex align-items-end justify-content-between">
+					    <span class="text-body-secondary timeSpan">56 minutes ago</span>
+					    <a href="#" class="viewBtn">view <i class="bi bi-chevron-right arrIcon ms-1"></i></a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- card end here -->
+			<!-- card start here -->
+			<div class="col-xxl-4 col-lg-4 col-md-6 col-12 mb-4">
+				<div class="card areCard">
+				  <div class="card-body">
+				    <h5 class="card-title areHead">Area Sales Manager</h5>
+				    <div class="d-flex align-items-center justify-content-between mb-4">
+				    	<span class="areSpan"><i class="bi bi-geo-alt" style="margin-right: 5px;"></i> Cork</span>
+				    	<span class="areSpan"><i class="bi bi-bag" style="margin-right: 5px;"></i> Permanent</span>
+				    	<span class="areSpan"><i class="bi bi-cash-stack" style="margin-right: 5px;"></i> Competitive</span>
+				    </div>
+				    <span class="areSpanN">new</span>
+				    <div class="d-flex align-items-end justify-content-between">
+					    <span class="text-body-secondary timeSpan">56 minutes ago</span>
+					    <a href="#" class="viewBtn">view <i class="bi bi-chevron-right arrIcon ms-1"></i></a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- card end here -->
+			<!-- card start here -->
+			<div class="col-xxl-4 col-lg-4 col-md-6 col-12 mb-4">
+				<div class="card areCard">
+				  <div class="card-body">
+				    <h5 class="card-title areHead">Area Sales Manager</h5>
+				    <div class="d-flex align-items-center justify-content-between mb-4">
+				    	<span class="areSpan"><i class="bi bi-geo-alt" style="margin-right: 5px;"></i> Cork</span>
+				    	<span class="areSpan"><i class="bi bi-bag" style="margin-right: 5px;"></i> Permanent</span>
+				    	<span class="areSpan"><i class="bi bi-cash-stack" style="margin-right: 5px;"></i> Competitive</span>
+				    </div>
+				    <span class="areSpanN">new</span>
+				    <div class="d-flex align-items-end justify-content-between">
+					    <span class="text-body-secondary timeSpan">56 minutes ago</span>
+					    <a href="#" class="viewBtn">view <i class="bi bi-chevron-right arrIcon ms-1"></i></a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- card end here -->
+			<!-- card start here -->
+			<div class="col-xxl-4 col-lg-4 col-md-6 col-12 mb-4">
+				<div class="card areCard">
+				  <div class="card-body">
+				    <h5 class="card-title areHead">Area Sales Manager</h5>
+				    <div class="d-flex align-items-center justify-content-between mb-4">
+				    	<span class="areSpan"><i class="bi bi-geo-alt" style="margin-right: 5px;"></i> Cork</span>
+				    	<span class="areSpan"><i class="bi bi-bag" style="margin-right: 5px;"></i> Permanent</span>
+				    	<span class="areSpan"><i class="bi bi-cash-stack" style="margin-right: 5px;"></i> Competitive</span>
+				    </div>
+				    <span class="areSpanN">new</span>
+				    <div class="d-flex align-items-end justify-content-between">
+					    <span class="text-body-secondary timeSpan">56 minutes ago</span>
+					    <a href="#" class="viewBtn">view <i class="bi bi-chevron-right arrIcon ms-1"></i></a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- card end here -->
+		</div>
+	</div>
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
